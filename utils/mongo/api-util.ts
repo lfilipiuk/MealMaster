c437c4f2c6509cc6bf8bbb77074a2b0fe5e76bfd
Lukasz <luke.filipiuk@gmail.com>
2023-02-13 20:53:01 +0100

    fixed fetch API

--- utils/mongo/api-util.ts
+++ utils/mongo/api-util.ts
@@ -1,5 +1,6 @@
 export async function getAllMeals() {
   //FIXME: localhost:3000 should be replaced with the actual URL of the API from environment variables
-  const res = await fetch("http://localhost:3000/api/meals");
+  const apiUrl = process.env.API_URL || "http://localhost:3000";
+  const res = await fetch(`${apiUrl}/api/meals`);
   return await res.json();
 }
