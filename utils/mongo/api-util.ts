bb7e41704d29e150a430946f716132c6c8ac48fb
Lukasz <luke.filipiuk@gmail.com>
2023-02-14 18:16:36 +0100

    fix for Vercel Serverless Function

--- utils/mongo/api-util.ts
+++ utils/mongo/api-util.ts
@@ -1,6 +1,10 @@
 export async function getAllMeals() {
-  //FIXME: localhost:3000 should be replaced with the actual URL of the API from environment variables
   const apiUrl = process.env.API_URL || "http://localhost:3000";
-  const res = await fetch(`${apiUrl}/api/meals`);
-  return await res.json();
+  try {
+    const res = await fetch(`${apiUrl}/api/meals`);
+    return await res.json();
+  } catch (error: any) {
+    if (error) console.error(`Error fetching meals: ${error.message}`);
+    throw error;
+  }
 }
