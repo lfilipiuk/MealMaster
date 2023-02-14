afe16f56e88053e4210d30dd90cc85e1b351bef0
Lukasz <luke.filipiuk@gmail.com>
2023-02-14 18:22:20 +0100

    fix for Vercel Serverless Function

--- utils/mongo/api-util.ts
+++ utils/mongo/api-util.ts
@@ -1,10 +1,12 @@
+import axios from "axios";
+
 export async function getAllMeals() {
   const apiUrl = process.env.API_URL || "http://localhost:3000";
   try {
-    const res = await fetch(`${apiUrl}/api/meals`);
-    return await res.json();
+    const response = await axios.get(`${apiUrl}/api/meals`);
+    return response.data;
   } catch (error: any) {
-    if (error) console.error(`Error fetching meals: ${error.message}`);
+    console.error(`Error fetching meals: ${error.message}`);
     throw error;
   }
 }
